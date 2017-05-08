65ab0832158810f96ee0b80864373623f141d94a
Magdalena Grabowska <mg372701@students.mimuw.edu.pl>
2017-05-08 20:51:44 +0200

    dodane linki przy kategoriach

---
+++ IO2017/src/main/java/io2017/quiz/ClosedQuizController.java
@@ -0,0 +1,53 @@
+package io2017.quiz;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io2017.categories.CategoriesRepository;
+import io2017.dictionaries.Dictionary;
+import io2017.dictionaries.DictionaryRepository;
+import io2017.dictionaries.Word;
+import io2017.dictionaries.WordRepository;
+import io2017.helpers.Language;
+import io2017.users.UserRepository;
+import io2017.users.UserRolesRepository;
+
+@Controller
+public class ClosedQuizController {
+	DictionaryRepository dictionaryRepository;
+	UserRepository userRepository;
+	CategoriesRepository categoriesRepository;
+	WordRepository wordRepository;
+	UserRolesRepository userRolesRepository;
+	
+	@Autowired
+	public ClosedQuizController(DictionaryRepository dictionaryRepository, 
+						UserRepository userRepository,
+						CategoriesRepository categoriesRepository,
+						WordRepository wordRepository,
+						UserRolesRepository userRolesRepository) {
+		this.dictionaryRepository = dictionaryRepository;
+		this.userRepository = userRepository;
+		this.categoriesRepository = categoriesRepository;
+		this.wordRepository = wordRepository;
+		this.userRolesRepository = userRolesRepository;
+	}
+	
+	@RequestMapping("/quiz/closed")
+	public String quizClosed(Model model, @RequestParam("id") long id) {
+			
+		Dictionary dictionary = dictionaryRepository.findOne(id);
+		List<Word> words = new LinkedList<Word>();
+		words.addAll(dictionary.getWords());
+		model.addAttribute("wordsNumber", words.size());
+		model.addAttribute("dictionaryLanguage", dictionary.getLanguage());
+		
+		return "quiz_closed";
+	}
+}
